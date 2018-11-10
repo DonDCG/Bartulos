9b981dbcfd888ec8c06b351f5056152924e0be69
DonDCG <38973587+DonDCG@users.noreply.github.com>
2018-11-10 21:19:41 +0100

    Botones dinámicos por BBDD

--- app/src/main/java/com/example/zerantus/bartulos/Inicio.java
+++ app/src/main/java/com/example/zerantus/bartulos/Inicio.java
@@ -1,11 +1,20 @@
 package com.example.zerantus.bartulos;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -15,8 +24,11 @@ import java.util.ListIterator;
 
 public class Inicio extends AppCompatActivity {
 
+    private static final String TAG = "Inicio" ;
     private ArrayList<Button> botonesIni;
     private Button mBtnLoc,mBtnRes,mBtnAloj,mBtnEvent,mBtnAjus,mBtnSalir;
+    private TextView label;
+    private DatabaseReference mBBDD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +41,27 @@ public class Inicio extends AppCompatActivity {
         mBtnEvent = findViewById(R.id.mBtnIni4);
         mBtnAjus = findViewById(R.id.mBtnIni5);
         mBtnSalir = findViewById(R.id.mBtnIni6);
+        label = (TextView) findViewById(R.id.mLocalidadLbl);
 
         botonesIni = new ArrayList<>();
         botonesIni.add(mBtnLoc);botonesIni.add(mBtnRes);botonesIni.add(mBtnAloj);
         botonesIni.add(mBtnEvent);botonesIni.add(mBtnAjus);botonesIni.add(mBtnSalir);
+
+        mBBDD = FirebaseDatabase.getInstance().getReference().
+                child("localidades").child("loc01").child("nombre");
+
+        mBBDD.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String valor = (String) dataSnapshot.getValue();
+                label.setText(valor);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "Error!", databaseError.toException());
+            }
+        });
 
         for (int i = 0;i<botonesIni.size();i++)
         {
--- app/src/main/java/com/example/zerantus/bartulos/Localizaciones.java
+++ app/src/main/java/com/example/zerantus/bartulos/Localizaciones.java
@@ -1,13 +1,93 @@
 package com.example.zerantus.bartulos;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Localizaciones extends AppCompatActivity {
 
+    private LinearLayout botonesLayout;
+    private static final String TAG = "Localiaciones" ;
+    private DatabaseReference mBBDD;
+    private TextView ejemplo;
+    private ValueEventListener eventListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localizaciones);
+
+        botonesLayout = (LinearLayout) findViewById(R.id.mContenedor);
+        /*botonesLayout = new LinearLayout(getApplicationContext());
+        botonesLayout.setLayoutParams(new LinearLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+        botonesLayout.setOrientation(LinearLayout.VERTICAL);
+        botonesLayout.setGravity(Gravity.CENTER);*/
+
+        mBBDD = FirebaseDatabase.getInstance().getReference().
+                child("localidades").child("loc01").child("localizaciones");
+
+        ejemplo = (TextView) findViewById(R.id.ejemplo);
+
+        eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Localizacion_BaseDatos pred = dataSnapshot.getValue(Localizacion_BaseDatos.class);
+                ejemplo.setText(pred.getNombre());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "Error!", databaseError.toException());
+            }
+        };
+        mBBDD.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //Obtenemos cada uno de los hijos y cargamos sus datos en botón
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
+                    Localizacion_BaseDatos post = postSnapshot.getValue(Localizacion_BaseDatos.class);
+                    ejemplo.setText(post.getNombre());
+                    Button boton = new Button(getApplicationContext());
+                    boton.setText(post.getNombre());
+                    boton.setBackgroundColor(Color.GREEN);
+
+                    boton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(getApplicationContext(), " Listener botón " + v.getTag(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    //Va agregegando botones al contenedor.
+                    botonesLayout.addView(boton);
+                    /*Localizacion_BaseDatos post = postSnapshot.getValue(Localizacion_BaseDatos.class);
+                    ejemplo.setText(post.getNombre());*/
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "Error!", databaseError.toException());
+            }
+        });
+
     }
 }
