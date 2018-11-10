22431321cd8dd7cfffe369d92016e05af5df23b0
DonDCG <38973587+DonDCG@users.noreply.github.com>
2018-11-10 12:14:27 +0100

    Listener Botones Inicio.class

--- app/src/main/java/com/example/zerantus/bartulos/Inicio.java
+++ app/src/main/java/com/example/zerantus/bartulos/Inicio.java
@@ -1,9 +1,11 @@
 package com.example.zerantus.bartulos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -21,24 +23,39 @@ public class Inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-        mBtnLoc = (Button) findViewById(R.id.mBtnIni1);
-        mBtnRes = (Button) findViewById(R.id.mBtnIni2);
-        mBtnAloj = (Button) findViewById(R.id.mBtnIni3);
-        mBtnEvent = (Button) findViewById(R.id.mBtnIni4);
-        mBtnAjus = (Button) findViewById(R.id.mBtnIni5);
-        mBtnSalir = (Button) findViewById(R.id.mBtnIni6);
+        mBtnLoc = findViewById(R.id.mBtnIni1);
+        mBtnRes = findViewById(R.id.mBtnIni2);
+        mBtnAloj = findViewById(R.id.mBtnIni3);
+        mBtnEvent = findViewById(R.id.mBtnIni4);
+        mBtnAjus = findViewById(R.id.mBtnIni5);
+        mBtnSalir = findViewById(R.id.mBtnIni6);
 
-        botonesIni = new ArrayList<Button>();
+        botonesIni = new ArrayList<>();
         botonesIni.add(mBtnLoc);botonesIni.add(mBtnRes);botonesIni.add(mBtnAloj);
         botonesIni.add(mBtnEvent);botonesIni.add(mBtnAjus);botonesIni.add(mBtnSalir);
 
-        for (Button boton:botonesIni)
+        for (int i = 0;i<botonesIni.size();i++)
         {
+            Button boton = botonesIni.get(i);
+            final int index = i;
             boton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    switch(botonesIni.get(1)){
-
+                    switch(index){
+                        case 0:startActivity(new Intent(Inicio.this,Localizaciones.class));
+                            break;
+                        case 1:startActivity(new Intent(Inicio.this,Restaurantes.class));
+                            break;
+                        case 2:startActivity(new Intent(Inicio.this,Alojamientos.class));
+                            break;
+                        case 3:startActivity(new Intent(Inicio.this,Eventos.class));
+                            break;
+                        case 4:startActivity(new Intent(Inicio.this,Ajustes.class));
+                            break;
+                        case 5:
+                            break;
+                        default: Toast.makeText(Inicio.this, "Error", Toast.LENGTH_SHORT).show();//TODO: Crear literal en strings
+                            break;
                     }
                 }
             });
