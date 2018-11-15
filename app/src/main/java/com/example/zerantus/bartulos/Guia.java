b93abb1ad89cdbd515ff12ad92b1ed19e89a97db
DonDCG <38973587+DonDCG@users.noreply.github.com>
2018-11-15 21:35:06 +0100

    X

---
+++ app/src/main/java/com/example/zerantus/bartulos/Guia.java
@@ -0,0 +1,16 @@
+package com.example.zerantus.bartulos;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Guia extends AppCompatActivity {
+
+    MediaPlayer reproductor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guia);
+    }
+}
--- app/src/main/java/com/example/zerantus/bartulos/Localizaciones.java
+++ app/src/main/java/com/example/zerantus/bartulos/Localizaciones.java
@@ -6,13 +6,9 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,9 +17,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Localizaciones extends AppCompatActivity {
 
@@ -38,15 +31,11 @@ public class Localizaciones extends AppCompatActivity {
         setContentView(R.layout.activity_localizaciones);
 
         botonesLayout = (LinearLayout) findViewById(R.id.mContenedor);
-        /*botonesLayout = new LinearLayout(getApplicationContext());
-        botonesLayout.setLayoutParams(new LinearLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
-        botonesLayout.setOrientation(LinearLayout.VERTICAL);
-        botonesLayout.setGravity(Gravity.CENTER);*/
 
         mBBDD = FirebaseDatabase.getInstance().getReference().
                 child("localidades").child("loc01").child("localizaciones");
 
-        ejemplo = (TextView) findViewById(R.id.ejemplo);
+        ejemplo = (TextView) findViewById(R.id.mLocTitulo);
 
         mBBDD.addValueEventListener(new ValueEventListener() {
             @Override
@@ -55,7 +44,6 @@ public class Localizaciones extends AppCompatActivity {
                 //Obtenemos cada uno de los hijos y cargamos sus datos en botón
                 for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
                     Localizacion_BaseDatos post = postSnapshot.getValue(Localizacion_BaseDatos.class);
-                    ejemplo.setText("A cambiar");
                     Button boton = new Button(getApplicationContext());
                     boton.setText(post.getNombre());
                     boton.setTextColor(Color.WHITE);
@@ -68,11 +56,11 @@ public class Localizaciones extends AppCompatActivity {
                     }
                     boton.setBackground(back);
                     boton.setWidth(90);
-
+                    final String ejemplo = (String) boton.getText();
                     boton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            Toast.makeText(getApplicationContext(), " Listener botón " + v.getTag(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), ejemplo, Toast.LENGTH_SHORT).show();
                         }
                     });
                     //Va agregegando botones al contenedor.
